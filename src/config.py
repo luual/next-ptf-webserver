b56cab8d9fcd018b80b044851236ce51b3a686b8
Alexandre Luu <alexandreluu@Alexandres-MacBook-Air.local>
2023-07-28 11:12:48 +0200

    update(config): remove database url

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
+import urllib
+
 class AppConfig:
     databaseUrl: str
 
     def __init__(self) -> None:
-        self.databaseUrl = "192.168.0.14:32768"
+        self.databaseUrl = ""
 
 appConfig = AppConfig()
